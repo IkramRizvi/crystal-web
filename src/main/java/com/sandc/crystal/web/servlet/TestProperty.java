e864cbb0221c2a172e384c7d62b3d41082db5e6d
Ikram Rizvi <Ikram.Rizvi@outlook.com>
2026-09-22 23:55:53 -0400

    Clean up TestProperty and tested config.properties between base and web.

--- src/main/java/com/sandc/crystal/web/servlet/TestProperty.java
+++ src/main/java/com/sandc/crystal/web/servlet/TestProperty.java
@@ -16,6 +16,14 @@ public class TestProperty extends HttpServlet {
 
                 String propertyId = (request.getParameter("property") != null) ? request.getParameter("property") : "default";
                 String propertyValue = "Property value not found";
+
+                String configFileServletContextPath = getServletContext().getRealPath("/WEB-INF/classes/default/config.properties");
+                String configFilePath = (request.getParameter("configFilePath") != null) ? request.getParameter("configFilePath") : configFileServletContextPath;
+
+                if (configFilePath == configFileServletContextPath) {
+                    ReadProperties.setPropertiesFile(configFileServletContextPath);
+                }
+
                 if (propertyId != "default") {
                     propertyId = propertyId.trim();
                     propertyValue = ReadProperties.getPropertyValue(propertyId);
@@ -23,6 +31,10 @@ public class TestProperty extends HttpServlet {
 
         response.setContentType("text/plain;charset=UTF-8");
         response.getWriter().write("The value of " + propertyId + " is: " + propertyValue);
+        response.getWriter().write("\nThe config file path is: " + configFilePath);
+        response.getWriter().flush();
+        response.getWriter().close();
+        
     }
     
 }

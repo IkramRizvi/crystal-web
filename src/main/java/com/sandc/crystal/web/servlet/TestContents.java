bcf9990fe346ab3f74133a8b1be22014f7ed74a4
Ikram Rizvi <Ikram.Rizvi@outlook.com>
2026-09-24 03:26:33 -0400

    Added TestContents servlet.

---
+++ src/main/java/com/sandc/crystal/web/servlet/TestContents.java
@@ -0,0 +1,42 @@
+package com.sandc.crystal.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestContents extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println(
+            "<HTML><head><title>Test Contents</title></head><body bgcolor=\"#FF5732\">"
+            + "<h1 align='center'>Display Header Request</h1>"
+            + "<b>Request Method:</b> " + request.getMethod() + "<br>"
+            + "<b>Request URI:</b> " + request.getRequestURI() + "<br>"
+            + "<b>Request Protocol:</b> " + request.getProtocol() + "<br><br>"
+            + "<table border='1' align='center'>"
+            + "<tr bgcolor=\"#FFAD00\">"
+            + "<th>Header Name</th><th>Header Value</th>"
+        );
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            out.println("<tr><td>" + headerName + "</td>");
+            out.println("<td>" + request.getHeader(headerName) + "</td></tr>");
+        }
+
+        out.println("</table></body></html>");
+    }
+    
+}
